778960c1bde5d66f981d10e302de7be1a06f0d45
Talent96 <121486742+Talent96@users.noreply.github.com>
2023-04-14 13:50:06 +0100

    Add files via upload

---
+++ secondHighestNumber.js
@@ -0,0 +1,18 @@
+function secondHighest(numbers) {
+    let highest = 0;
+    let secondHighest = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] > highest) {
+        secondHighest = highest;
+        highest = numbers[i];
+      } else if (numbers[i] < highest && numbers[i] > secondHighest) {
+        secondHighest = numbers[i];
+      }
+    }
+    
+    return secondHighest;
+  }
+  
+  console.log(secondHighest([21,28,10,4,40,40])); // Output: 5
+  
